07aa4709b1ac5ac0a0cd6c3b528034d86225c584
AndriyBarskyi <barskyiandriy@gmail.com>
2023-08-28 12:50:08 +0300

    refactor and extend EquationSolverTest

--- src/test/java/com/example/equations/EquationSolverTest.java
+++ src/test/java/com/example/equations/EquationSolverTest.java
@@ -23,18 +23,11 @@ class EquationSolverTest {
             Arguments.of("2*x + 6 = 18", 6.0),
             Arguments.of("x*x - 5x + 6 = 0", 2.0),
             Arguments.of("x*x - 5x + 6 = 0", 3.0),
-            Arguments.of("x - 5x + 6 = -2", 2.0)
+            Arguments.of("x - 5x + 6 = -2", 2.0),
+            Arguments.of("6 + 6 = 2 * x", 6.0),
+            Arguments.of("15.6 / 2.0 + 2.0 = 2.0 + x", 7.8),
+            Arguments.of("16.4 / (2.0 + 2.0) = 2.0 + x", 2.1)
         );
-    }
-
-    @Test
-    void testCalculateEquationValueWithInvalidEquation() {
-        String invalidEquation = "2*x + ";
-        double xValue = 4.0;
-
-        assertThrows(IllegalArgumentException.class, () -> {
-            EquationSolver.calculateEquationValue(invalidEquation, xValue);
-        }, "IllegalArgumentException should be thrown for invalid equation");
     }
 }
 
